e219c80b8a56be28bbc03a6234990fab5481e2da
engmoha1717 <engmoha1717@gmail.com>
2025-02-09 06:29:03 -0800

    layout website

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const APP_NAME=process.env.NEXT_PUBLIC_APP_NAME || 'NxtAmzn'
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'An  Amazon clone built with Next.js and mongoDB'
+
+export const USER_ROLES = ['Admin', 'User']
+export const COLORS = ['Gold', 'Green', 'Red']
+export const THEMES = ['Light', 'Dark', 'System']
