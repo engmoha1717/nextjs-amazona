81abf49b854e1cf7e694002cc2b6555b98d798cf
engmoha1717 <engmoha1717@gmail.com>
2025-02-10 23:34:46 -0800

    browsing history

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -10,6 +10,7 @@ import ProductGallery from '@/components/shared/product/product-gallery'
 import { Separator } from '@/components/ui/separator'
 import ProductSlider from '@/components/shared/product/product-slider'
 import Rating from '@/components/shared/product/rating'
+import BrowsingHistoryList from '@/components/shared/browsing-history-list'
 
 export async function generateMetadata(props: {
   params: Promise<{ slug: string }>
@@ -122,6 +123,9 @@ export default async function ProductDetails(props: {
           title={`Best Sellers in ${product.category}`}
         />
       </section>
+      <section>
+     <BrowsingHistoryList className='mt-10' />
+   </section>
     </div>
   )
 }
