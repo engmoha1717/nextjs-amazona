06df75e1714385257d5c32496c50034f7de661cb
engmoha1717 <engmoha1717@gmail.com>
2025-02-11 05:11:16 -0800

    add to cart

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,10 @@
-import { ProductInputSchema } from '@/lib/validator'
-    import { z } from 'zod'
 
+    import { z } from 'zod'
+  import {
+      CartSchema,
+      OrderItemSchema,
+      ProductInputSchema,
+    } from '@/lib/validator'
     export type IProductInput = z.infer<typeof ProductInputSchema>
     export type Data = {
       products: IProductInput[]
@@ -16,3 +20,7 @@ import { ProductInputSchema } from '@/lib/validator'
         isPublished: boolean
       }[]
     }
+
+  
+    export type OrderItem = z.infer<typeof OrderItemSchema>
+    export type Cart = z.infer<typeof CartSchema>
